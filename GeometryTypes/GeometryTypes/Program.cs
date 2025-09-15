d53d98d83aa47bc98a4839eedf693474743a7b05
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-15 22:07:25 +0700

    Code Geometry Types

---
+++ GeometryTypes/GeometryTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GeometryTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program : Display Geometry Types");
+            Console.WriteLine("===================================");
+            Console.WriteLine("1. Print the rectangle");
+            Console.WriteLine("2. Print the square triangle");
+            Console.WriteLine("3. Print isosceles triangle");
+            Console.WriteLine("4. Exit");
+
+        }
+    }
+}
