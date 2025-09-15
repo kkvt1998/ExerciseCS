65ae117853db216678c86808425fc3697be13fcc
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-16 06:31:12 +0700

    push

--- GeometryTypes/GeometryTypes/Program.cs
+++ GeometryTypes/GeometryTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters;
 
 namespace GeometryTypes
 {
@@ -58,6 +59,98 @@ namespace GeometryTypes
                         Console.Write("Length b: ");
                         int b = int.Parse(in_put());
                         rectangle(a, b);
+                        break;
+                    //show triagle types
+                    case 2:
+                        Console.WriteLine("Print the square triangle");
+                        Console.Write("Enter length of side: ");
+                        int c = int.Parse(in_put());
+                        Console.WriteLine("1. Top-left");
+                        Console.WriteLine("2. Top-right");
+                        Console.WriteLine("3. Bottom-left");
+                        Console.WriteLine("4. Bottom-right");
+                        Console.Write("Enter your choice: ");
+                        int TriagleChoice = int.Parse(in_put());
+                        //top-left
+                        if (TriagleChoice == 1)
+                        {
+                            for (int i = 0; i < c; i++) {
+                                for (int j = c; j > i; j--) {
+                                    Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        //top-right
+                        else if (TriagleChoice == 2)
+                        {
+                            for (int i = 0; i < c; i++)
+                            {
+                                for (int j = 0; j < i; j++)
+                                {
+                                    Console.Write("  ");
+                                }
+                                for (int k = c; k > i; k--)
+                                {
+                                    Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        //bottom-left
+                        else if (TriagleChoice == 3)
+                        {
+                            for (int i = 0; i < c; i++)
+                            {
+                                for (int j = 0; j < i; j++)
+                                {
+                                    Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        //bottom-right
+                        else if (TriagleChoice == 4)
+                        {
+                            for (int i = 0; i < c; i++)
+                            {
+                                for (int j = c; j > i; j--)
+                                {
+                                    Console.Write("  ");
+                                }
+                                for (int k = 0; k < i; k++)
+                                {
+                                    Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        else 
+                        {
+                            Console.WriteLine("No choice!");
+                        }
+                            break;
+                    case 3:
+                        Console.WriteLine("Print isosceles triangle ");
+                        Console.Write("Enter length of side: ");
+                        int d = int.Parse(in_put());
+                        for (int i = 0; i < d; i++)
+                        {
+                            for (int j = d; j > i; j--)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int k = 0; k < 2*i - 1; k++)
+                            {
+                                Console.Write("*");
+                            }
+                            
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 4: 
+                        Console.WriteLine("Exit");
+                        choice = 0;
                         break;
                     default:
                         break;
