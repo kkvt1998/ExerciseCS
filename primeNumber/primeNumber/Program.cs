5ed65cb5eee573c80a0783a0bf0cbe9ccaf42d97
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-17 20:44:31 +0700

    Code primeNumber

---
+++ primeNumber/primeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace primeNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program: dislay prime numbers between 2 to 100");
+            int dem = 0;
+            for(int i = 2; i < 100; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        dem = dem + 1;
+                    }
+                }
+                if(dem == 2)
+                {
+                    Console.WriteLine(i);
+                    
+                }
+                dem = 0;
+            }
+        }
+    }
+}
