7845e7c64495691fcd9b449d07f8842779d2eba7
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-17 21:53:50 +0700

    Code greatest Common Divisor

---
+++ greatestCommonDivisor/greatestCommonDivisor/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace greatestCommonDivisor
+{
+    class Program
+    {
+        public static int in_put()
+        {
+            string x = "";
+            int result = 0;
+            bool check = false;
+            while (!check)
+            {
+                x = Console.ReadLine();
+                if(int.TryParse(x, out result))
+                {
+                    check = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Value, please re-enter!");
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program: Find the greatest common divisor ");
+            int number1, number2;
+            Console.WriteLine("Enter the first number: ");
+            number1 = Math.Abs(in_put());
+            Console.WriteLine("Enter the second number: ");
+            number2 = Math.Abs(in_put());
+            if(number1 == 0 && number2 == 0)
+            {
+                Console.WriteLine("No greatest common divisor exists!");
+            }
+            else if(number1 == 0 || number2 == 0) 
+            {
+                Console.WriteLine(" The greatest Common Divisor is: " + (number1 + number2));
+
+            }
+            else if(number1 == number2)
+            {
+                Console.WriteLine(" The greatest Common Divisor is: " + number1);
+
+            }
+            else {
+                int min;
+                if (number1 < number2)
+                {
+                    min = number1;
+                }
+                else
+                {
+                    min = number2;
+                }
+                for(int i = min; i >= 1; i--)
+                {
+                    if(number1 % i == 0 && number2 % i == 0)
+                    {
+                        Console.WriteLine("The greatest Common Divisor is " + i);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
