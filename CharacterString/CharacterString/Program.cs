f97ab846107534ed8ff34ee0a516de7295a39baa
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-25 21:12:10 +0700

    Code Count character in String

---
+++ CharacterString/CharacterString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CharacterString
+{
+    class Program
+    {
+        public static int count_character(string x, string a)
+        {
+            int count = 0;
+            for(int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == Convert.ToChar(a))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program: count of the number character in a string");
+            Console.WriteLine("Enter a sentence: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Enter the character to find: ");
+            string char_count = Console.ReadLine();
+            int count = count_character(str, char_count);
+            Console.WriteLine("Character " + char_count + " appears " + count + " times"); 
+        }
+    }
+}
