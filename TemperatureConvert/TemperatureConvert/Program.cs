76ba53adea47f9bba9587d409065b00671f1a547
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-25 22:42:36 +0700

    Code temperature convert

---
+++ TemperatureConvert/TemperatureConvert/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace TemperatureConvert
+{
+    class Program
+    {
+    
+        public static void convertCtoF()
+        {
+            Console.WriteLine("Please enter the degree C: ");
+            double degree = (double)in_put();
+            degree = degree * 9 / 5 + 32;
+            Console.WriteLine("Result: " + degree + "F");
+        }
+        public static void convertFtoC()
+        {
+            Console.WriteLine("Please enter the degree F: ");
+            double degree = (double)in_put();
+            degree = (degree - 32)* 5/9;
+            Console.WriteLine("Result: " + degree + "C");
+        }
+        public static int in_put()
+        {
+            string x = "";
+            int result = 0;
+            bool check = false;
+            while (!check)
+            {
+                x = Console.ReadLine();
+                if (int.TryParse(x, out result))
+                {
+                    check = true;
+                }
+                else
+                    Console.WriteLine("Input invalid, please re-enter!");
+            }
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program: Temperature Convert");
+            bool access = false;
+            while (!access)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Convert degree C to degree F");
+                Console.WriteLine("2. Convert degree F to degree C");
+                Console.WriteLine("3. Exit");
+                int choose = in_put();
+                if (choose == 3)
+                {
+                    access = true;
+                }
+                else if (choose == 1)
+                {
+                    convertCtoF();
+                }
+                else if (choose == 2)
+                {
+                    convertFtoC();
+                }
+                else
+                {
+                    Console.WriteLine("input invalid, please re-choose !");
+                }
+                
+            }
+            
+
+        }
+    }
+}
