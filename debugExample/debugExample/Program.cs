4cfccbc035378d2197736626c3f69533d6e1a9d7
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-26 21:48:36 +0700

    First commit to debug

---
+++ debugExample/debugExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace debugExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for(int i =0; i < 10; i++)
+            {
+                Console.WriteLine("Index = " + i);
+            }
+        }
+    }
+}
